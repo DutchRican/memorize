19cf3c92d58b373098c5906f6deb1e95600566af
Paul van Woensel <pvanwoensel@gmail.com>
2023-10-12 14:52:27 -0500

    initial commit

---
+++ Memorize/CardView.swift
@@ -0,0 +1,30 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by Paul van Woensel on 10/12/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth: 4)
+                Text("👻").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        }
+    }
+}
+
+#Preview {
+    CardView()
+}
